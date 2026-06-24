ddee5f75c5ed7f8dec1f9dc923d069343f1e04be
Olugbemiga Ojumu <olugbemiga.ojumu@gmail.com>
2026-06-24 13:22:47 +0200

    Added temperature_converter.py

---
+++ temperature_converter.py
@@ -0,0 +1,13 @@
+def celsius_to_fahrenheit(temp):
+    fahrenheit = (temp * 9 / 5) + 32
+    return fahrenheit
+
+
+def main():
+    celsius_to_fahrenheit(37)
+
+
+if __name__ == "__main__":
+    main()
+
+
